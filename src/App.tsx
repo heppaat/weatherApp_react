4e2d5ede4b073843de36b37e0308506ea3a97d9e
heppaat <pali.herrer@gmail.com>
2024-12-09 19:11:58 +0100

    chore(vite): vite and tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-screen justify-center items-center">
+        <h1 className="bg-slate-500 w-full text-center">weatherapp</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
